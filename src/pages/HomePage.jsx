dd645ced82dd2a8edb5114151e09376b5d654a3d
Elizabeth <liz4momentum@gmail.com>
2023-06-01 09:41:34 -0600

    Title text with shadow in position

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,6 +5,10 @@ export default function HomePage() {
 
   return (
     <div className="container-fluid nebula-bg">
+      <div className="d-flex justify-content-center">
+        <h1 className="bungee-font white-title mt-5">MOON MINER</h1>
+        <h1 className="bungee-font yellow-title">MOON MINER</h1>
+      </div>
     </div>
   )
 }
